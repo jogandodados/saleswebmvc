befc5bc9c5e20e87f19e4472ffe5c962c83169ce
jogandodados <pedro.robertofiori@gmail.com>
2020-01-24 12:30:02 -0300

    First Model-Controller-View - Department

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using SalesWebMvc.Models;
+using SalesWebMvc.Models.ViewModels;
 
 namespace SalesWebMvc.Controllers
 {
@@ -17,8 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-            ViewData["email"] = "pedro.robertofiori@gmail.com";
+            ViewData["Message"] = "Salles Web Mvc app from C# Course";
+            ViewData["Aluno"] = "Pedro Fiori";
 
             return View();
         }
